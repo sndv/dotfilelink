92610822489090a99f9c446a140f92c312bbb09a
sndv <sndv@mailbox.org>
2021-03-13 21:12:49 +0200

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+VERSION = "0.1.0"
+
+setuptools.setup(
+    name="dotfilelink",
+    version=VERSION,
+    author="sndv",
+    author_email="sndv@mailbox.org",
+    description="A tool to link or copy dotfiles",
+    url="https://github.com/sndv/dotfilelink",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+    ],
+    packages=["dotfilelink"],
+    python_requires=">=3.8",
+    install_requires=[
+        "PyYAML>=5.4",
+    ],
+    entry_points = {
+        "console_scripts": [
+            "dotfilelink = dotfilelink.dotfilelink:main",
+        ],
+    },
+)
